1fa6f907652c6d7ba035d136f6bd98bafccd7405
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-12-12 20:38:16 +0530

    Twitter

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/twitter", (req, res) => {
+  res.send("<a href='https://twitter.com/0xSaksham'>Twitter-0xSaksham</a>");
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
